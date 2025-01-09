ed766c9b687e448946e64c2cb9d0eae1ed6dd243
Sean Nam <blt24q4@gmail.com>
2025-01-09 10:25:07 +0900

    가입 폼 작업

---
+++ src/main/java/himedia/kdt/myhome/controllers/UserServlet.java
@@ -0,0 +1,24 @@
+package himedia.kdt.myhome.controllers;
+
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class UserServlet extends BaseServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String actionName = req.getParameter("a");
+		
+		if ("joinform".equals(actionName)) {
+			//	가입 폼 보여주기
+			RequestDispatcher rd =
+				req.getRequestDispatcher("/WEB-INF/views/users/joinform.jsp");
+			rd.forward(req, resp);
+		}
+	}
+
+}
